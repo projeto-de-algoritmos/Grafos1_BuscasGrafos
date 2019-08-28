1d1ab47ee613443d2f6899cb0e799be943508edb
joaovitorml <joao.lemos4000@gmail.com>
2019-08-28 20:39:21 -0300

    Esqueleto inicial do dfs

--- Trabalho1/dfs.py
+++ Trabalho1/dfs.py
@@ -2,8 +2,27 @@ import time
 
 inicio = time.time()
 
+lista = ['1:[2]','2:[3]','3:[4]','4:[1]']
+arvore = []
+explorados = []
+
+def dfs(lista):
+    for v in lista[1]:
+        if v not in explorados:
+            dfs_visit(lista,v)
 
 
+def dfs_visit(lista,v):
+    explorados.append(v)
+    for w in lista[2]:
+        if w  not in explorados:
+            temp = [v,':[',w,']']
+            arvore.append(temp)
+            dfs_visit(lista,w)
+
+dfs(lista)
+
+print(explorados)
 
 
 fim = time.time()

41a79e2990bdc367335e34bd2529055ca793b710
joaovitorml <joao.lemos4000@gmail.com>
2019-08-29 21:40:26 -0300

    dfs funcionando.
    Co-authored-by: FranciscoHeronildo <heronildo.2010@hotmail.com>

--- Trabalho1/dfs.py
+++ Trabalho1/dfs.py
@@ -2,28 +2,30 @@ import time
 
 inicio = time.time()
 
-lista = ['1:[2]','2:[3]','3:[4]','4:[1]']
+lista = {'1':['2'],'2':['3'],'3':['4'],'4':['1']}
 arvore = []
 explorados = []
 
 def dfs(lista):
-    for v in lista[1]:
+    for v in lista:
         if v not in explorados:
+            print(v)
             dfs_visit(lista,v)
 
 
 def dfs_visit(lista,v):
     explorados.append(v)
-    for w in lista[2]:
+    for w in lista:
         if w  not in explorados:
+            print(w)
             temp = [v,':[',w,']']
             arvore.append(temp)
             dfs_visit(lista,w)
 
 dfs(lista)
 
-print(explorados)
-
+print('explorados: ', explorados)
+print('arvore: ', arvore)
 
 fim = time.time()
 

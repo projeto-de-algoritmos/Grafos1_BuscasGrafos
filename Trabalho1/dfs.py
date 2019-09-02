2196a5ba6c8403624329b9b4c16c36f271b0b82c
joaovitorml <joao.lemos4000@gmail.com>
2019-09-02 20:38:58 -0300

    Arrumando pequenos erros de sintaxe.
    Co-authored-by: FranciscoHeronildo <heronildo.2010@hotmail.com>

--- Trabalho1/dfs.py
+++ Trabalho1/dfs.py
@@ -9,7 +9,7 @@ inicio = time.time()
 # Caso 3
 #lista = {'1':['2','3','4','5'],'2':['1','3','4','5'],'3':['1','2','4','5'],'4':['1','2','3','5'],'5':['1','2','3','4']}
 # Caso 4
-#lista = {'3':['1'],'7':['1','2','3','5','6','9'],'5':['1','2','3','6','7','9'],'2':['1','3','5','6','7','9'],'1':[,'2','3','5','6','7','9'],'9':['1','2','3','5','6','7',], '6':['1','2','3','5','7','9']}
+#lista = {'3':['1'],'7':['1','2','3','5','6','9'],'5':['1','2','3','6','7','9'],'2':['1','3','5','6','7','9'],'1':['2','3','5','6','7','9'],'9':['1','2','3','5','6','7',], '6':['1','2','3','5','7','9']}
 # Caso 5
 #lista = {'3':['6'],'6':['9'],'9':['3']}
 
@@ -28,14 +28,14 @@ def dfs(lista):
 # faz a busca por profundidade recursivamente
 def dfs_visit(lista,v):
     explorados.append(v)
-    # chama a função de busca para cada adjacente de v
+    # chama a funcao de busca para cada adjacente de v
     for w in lista[v]:
         if w not in explorados:
             temp = [v,':[',w,']']
             arvore.append(temp)
             dfs_visit(lista,w)
 
-# chamando a função
+# chamando a funcao
 dfs(lista)
 
 # tempo
@@ -45,4 +45,4 @@ print('explorados: ', explorados)
 
 print('arvore: ', arvore)
 
-print('tempo de execução: ', fim - inicio)
+print('tempo de execucao: ', fim - inicio)
